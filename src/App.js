a6f3b7f0a4a1ad7907753c48356c1f6b247c09f1
YoungArthas <ivansavchenko200@gmail.com>
2023-12-07 15:52:19 +0200

    edit v4

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> for Ivan Savchenko! Change 3rd!
+          Edit <code>src/App.js</code> for Ivan Savchenko + Jenkins!
         </p>
         <a
           className="App-link"
